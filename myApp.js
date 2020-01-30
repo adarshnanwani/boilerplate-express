2bfc91481a7245976afbabeddaea582f5c80c22d
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-01-30 19:14:02 +0530

    Lesson 6 - add env vars

--- myApp.js
+++ myApp.js
@@ -29,7 +29,12 @@ app.use(express.static(__dirname + '/public/'));
 
 /** 5) serve JSON on a specific route */
 app.get('/json', function(req,res) {
-  res.json({"message": "Hello json"});
+  if(process.env.MESSAGE_STYLE === uppercase){
+  res.json({ message: "HELLO JSON" });
+  } else {
+  res.json({ message: "Hello json" });
+  }
+
 })
 
 
